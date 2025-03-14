6a9a677929722991e86e2c48c3263d3a8ba165f1
annabelowigho <113777360+annaelowigho@users.noreply.github.com>
2025-03-14 14:56:02 +0100

    for customers section

--- src/Components/ForCustomers.jsx
+++ src/Components/ForCustomers.jsx
@@ -1,24 +1,38 @@
 import React from 'react'
 import { MdOutlineAccessTime } from "react-icons/md";
 
-const Cus = [
+const Features = [
     {
         icon: MdOutlineAccessTime,
         title: "Save Time",
         description: "Save Time Skip the hassle of doing laundry yourself. Our platform connects you to professional services."
     },
-    {
-        icon: MdOutlineAccessTime,
-        title: "Save Time",
-        description: "Save Time Skip the hassle of doing laundry yourself. Our platform connects you to professional services."
-    }
+    // {
+    //     icon: MdOutlineAccessTime,
+    //     title: "Save Time",
+    //     description: "Save Time Skip the hassle of doing laundry yourself. Our platform connects you to professional services."
+    // }
 ]
 
 const ForCustomers = () => {
   return (
     <>
-        <h1>For Customers</h1>
-        <p>Clean makes your laundry experience hassle-free with these great features</p>
+        <div className='px-5 md:px-[50px] lg:px-[100px] pt-[50px] md:pt-[100px] xl:pt-[150px] flex flex-col items-center'>
+          <h1 className='bg-gradient-to-r from-[#1f9df4] to-[#1fd9ba] text-center bg-clip-text text-transparent text-[30px] md:text-[50px] font-bold'>
+            For Customers
+          </h1>
+          <p className='pt-2 text-[15px] md:text-[20px] w-full md:w-[600px] text-[#69778f] text-center'>Clean makes your laundry experience hassle-free with these great features</p>
+        </div>
+
+        <div>
+          {Features.map((Features, index) => (
+            <div key={index}>
+              <span>{Features.icon}</span>
+              <h4>{Features.title}</h4>
+              <p>{Features.description}</p>
+            </div>
+          ))}
+        </div>
     </>
   )
 }
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -8,7 +8,7 @@ const Homepage = () => {
       <div className='px-5 md:px-[50px] lg:px-[100px] py-[30px] md:py-[30px] flex flex-col lg:flex-row items-center gap-10 lg:gap-0'>
         <div className='w-full lg:w-1/2 flex flex-col justify-center'>
           <h1 className='text-[60px] md:text-[70px] font-bold w-full md:w-[700px] leading-[60px] md:leading-[75px]'>
-            Connecting <span className='text-[#1f9df4]'>Laundry</span> <span className='text-[#1fd9ba]'>Shops</span> With <span className='gradient-text'>Customers</span>
+            Connecting <span className='text-[#1f9df4]'>Laundry</span> <span className='text-[#1fd9ba]'>Shops</span> With <span className='bg-gradient-to-r from-[#1f9df4] to-[#1fd9ba] bg-clip-text text-transparent'>Customers</span>
           </h1>
           <p className='pt-6 text-[20px] w-full md:w-[500px] text-[#69778f]'>
             Clean makes laundry easier by connecting you to local laundry shops. Save time and hassle with our simple platform.
