c458ecceb7bc24c27ba3410086cc9d66882d9d2d
annabelowigho <113777360+annaelowigho@users.noreply.github.com>
2025-03-19 16:06:18 +0100

    about page corrected

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -41,7 +41,7 @@ const AboutUs = () => {
                 <h1 className='bg-gradient-to-r from-[#1f9df4] to-[#1fd9ba] text-center bg-clip-text text-transparent text-[30px] md:text-[50px] font-bold'>
                     Mission
                 </h1>
-                <div className='flex flex-col items-center xl:flex-row py-5 xl:py-10 gap-6'>
+                <div className='flex flex-col items-center xl:flex-row-reverse py-5 xl:py-10 gap-6'>
                     <p className='pt-2 text-[15px] md:text-[30px] w-full xl:w-1/2 text-[#69778f] text-justify md:text-center'>
                         At Clean, our mission is to simplify laundry care through innovation, accessibility, and efficiency—helping 
                         individuals and businesses focus on what matters while we take care of the rest.
