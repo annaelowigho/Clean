02fb85fb9527cfa35f909bcc0619933a15aaa829
annabelowigho <113777360+annaelowigho@users.noreply.github.com>
2025-03-24 12:22:33 +0100

    corrections made

--- src/Components/Partners.jsx
+++ src/Components/Partners.jsx
@@ -10,7 +10,7 @@ const Partners = () => {
       <motion.div
         className="flex space-x-6 w-[200%]"
         animate={{ x: ["0%", "-50%"] }}
-        transition={{ repeat: Infinity, duration: 10, ease: "linear" }}
+        transition={{ repeat: Infinity, duration: 15, ease: "linear" }}
       >
         {[...Logos, ...Logos, ...Logos].map((logo, index) => (
           <img key={index} src={logo} alt={`Logo ${index + 1}`} className="h-20 w-auto" />
